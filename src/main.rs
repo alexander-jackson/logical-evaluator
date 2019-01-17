1c1170b7fd74b992274c9aeab39262862885aded
Alexander Jackson <alexanderjackson@protonmail.com>
2019-01-17 15:07:59 +0000

    Add evaluation algorithm to evaluate the AST

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use std::env;
 use std::collections::HashMap;
 
-fn shunting_yard(expression: String) {
+fn shunting_yard(expression: String) -> String {
     let mut output: Vec<char> = Vec::new();
     let mut stack: Vec<char> = Vec::new();
 
@@ -12,9 +12,6 @@ fn shunting_yard(expression: String) {
     ].iter().cloned().collect();
 
     for c in expression.chars() {
-        println!("{}", c);
-        println!("Alphabetic: {}", c.is_alphabetic());
-
         if c.is_alphabetic() {
             output.push(c);
         } else {
@@ -30,12 +27,58 @@ fn shunting_yard(expression: String) {
         output.push(stack.pop().unwrap());
     }
 
-    println!("{:?}", output);
-    println!("{:?}", stack);
-
     let parsed: String = output.into_iter().collect();
 
-    println!("{}", parsed);
+    return parsed;
+}
+
+fn get_value(atom: char, map: &HashMap<char, bool>) -> bool {
+    return match map.get(&atom) {
+        Some(_v) => true,
+        None => false
+    };
+}
+
+fn evaluate(ast: String, valuation: String) -> bool {
+    let mut stack: Vec<char> = Vec::new();
+    let mut values: HashMap<char, bool> = HashMap::new();
+
+    for c in valuation.chars() {
+        values.insert(c, true);
+    }
+
+    values.insert('T', true);
+
+    for c in ast.chars() {
+        if c.is_alphabetic() {
+            stack.push(c);
+        } else {
+            let op = c;
+
+            let a: bool = get_value(stack.pop().unwrap(), &values);
+            let b: bool = get_value(stack.pop().unwrap(), &values);
+
+            let mut val: bool = false;
+
+            if op == '&' {
+                val = a & b;
+            } else if op == '|' {
+                val = a | b;
+            }
+
+            if val {
+                stack.push('T');
+            } else {
+                stack.push('F');
+            }
+        }
+    }
+
+    if stack.pop().unwrap() == 'T' {
+        return true;
+    } else {
+        return false;
+    }
 }
 
 fn main() {
@@ -52,4 +95,5 @@ fn main() {
     println!("Valuation: {}", valuation);
 
     let ast = shunting_yard(expression.to_string());
+    println!("Evaluation: {}", evaluate(ast, valuation.to_string()));
 }
