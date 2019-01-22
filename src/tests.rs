46c3eb42f69e3a9b08bc399cf7c5921264665957
Alexander Jackson <alexanderjackson@protonmail.com>
2019-01-22 09:13:54 +0000

    Add tests for implication operator

--- src/tests.rs
+++ src/tests.rs
@@ -25,6 +25,14 @@ fn shunting_yard_not_test() {
 }
 
 #[test]
+fn shunting_yard_implication_test() {
+    let input = "p>q".to_owned();
+    let output = shunting_yard(&input);
+    let expected = "pq>";
+    assert_eq!(output, expected);
+}
+
+#[test]
 fn shunting_yard_and_then_or_test() {
     let input = "p&q|r".to_owned();
     let output = shunting_yard(&input);
@@ -120,6 +128,40 @@ fn evaluate_not_operation_test() {
     assert_eq!(output, expected);
 
     valuation.insert('p', false);
+
+    let output = evaluate(&input, &valuation);
+    let expected = true;
+    assert_eq!(output, expected);
+}
+
+#[test]
+fn evaluate_implication_operation_test() {
+    let input = "pq>".to_owned();
+    let mut valuation = HashMap::new();
+
+    valuation.insert('p', true);
+    valuation.insert('q', true);
+
+    let output = evaluate(&input, &valuation);
+    let expected = true;
+    assert_eq!(output, expected);
+
+    valuation.insert('p', true);
+    valuation.insert('q', false);
+
+    let output = evaluate(&input, &valuation);
+    let expected = false;
+    assert_eq!(output, expected);
+
+    valuation.insert('p', false);
+    valuation.insert('q', true);
+
+    let output = evaluate(&input, &valuation);
+    let expected = true;
+    assert_eq!(output, expected);
+
+    valuation.insert('p', false);
+    valuation.insert('q', false);
 
     let output = evaluate(&input, &valuation);
     let expected = true;
