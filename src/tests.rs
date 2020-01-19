1f9b807d2b7331406e29ba2c9cf88f11a8b1c6c1
Alexander Jackson <alexanderjackson@protonmail.com>
2020-01-19 20:32:38 +0000

    Cleanup tests with a macro

--- src/tests.rs
+++ src/tests.rs
@@ -1,100 +1,28 @@
 use super::*;
 
-#[test]
-fn shunting_yard_only_two_and_test() {
-    let input = "p&q".to_owned();
-    let output = shunting_yard(&input);
-    let expected = "pq&";
-    assert_eq!(output, expected);
+macro_rules! shunting_yard {
+    ($($name:ident: $input:expr, $expected:expr,)*) => {
+        $(
+            #[test]
+            fn $name() {
+                let input: &str = $input;
+                let output: &str = &shunting_yard(input);
+                let expected: &str = $expected;
+                assert_eq!(output, expected);
+            }
+        )*
+    }
 }
 
-#[test]
-fn shunting_yard_only_two_or_test() {
-    let input = "p|q".to_owned();
-    let output = shunting_yard(&input);
-    let expected = "pq|";
-    assert_eq!(output, expected);
-}
-
-#[test]
-fn shunting_yard_not_test() {
-    let input = "!p".to_owned();
-    let output = shunting_yard(&input);
-    let expected = "p!";
-    assert_eq!(output, expected);
-}
-
-#[test]
-fn shunting_yard_implication_test() {
-    let input = "p>q".to_owned();
-    let output = shunting_yard(&input);
-    let expected = "pq>";
-    assert_eq!(output, expected);
-}
-
-#[test]
-fn shunting_yard_and_then_or_test() {
-    let input = "p&q|r".to_owned();
-    let output = shunting_yard(&input);
-    let expected = "pq&r|";
-    assert_eq!(output, expected);
-}
-
-#[test]
-fn shunting_yard_or_then_and_test() {
-    let input = "p|q&r".to_owned();
-    let output = shunting_yard(&input);
-    let expected = "pqr&|";
-    assert_eq!(output, expected);
-}
-
-#[test]
-fn shunting_yard_with_spaces_test() {
-    let input = "p & q".to_owned();
-    let output = shunting_yard(&input);
-    let expected = "pq&";
-    assert_eq!(output, expected);
-
-    let input = "p | q".to_owned();
-    let output = shunting_yard(&input);
-    let expected = "pq|";
-    assert_eq!(output, expected);
-
-    let input = "p > q".to_owned();
-    let output = shunting_yard(&input);
-    let expected = "pq>";
-    assert_eq!(output, expected);
-
-    let input = "p & q | r".to_owned();
-    let output = shunting_yard(&input);
-    let expected = "pq&r|";
-    assert_eq!(output, expected);
-
-    let input = "p | q & r".to_owned();
-    let output = shunting_yard(&input);
-    let expected = "pqr&|";
-    assert_eq!(output, expected);
-}
-
-#[test]
-fn shunting_yard_long_implication_test() {
-    let input = "p=>q".to_owned();
-    let output = shunting_yard(&input);
-    let expected = "pq>";
-    assert_eq!(output, expected);
-
-    let input = "p => q".to_owned();
-    let output = shunting_yard(&input);
-    let expected = "pq>";
-    assert_eq!(output, expected);
-}
-
-#[test]
-fn shunting_yard_long_implication_spaces_test() {
-    let input = "p = > q".to_owned();
-    let output = shunting_yard(&input);
-    let expected = "pq>";
-    assert_eq!(output, expected);
+shunting_yard! {
+    logical_and: "p&q", "pq&",
+    logical_or: "p|q", "pq|",
+    logical_not: "!p", "p!",
+    implication: "p>q", "pq>",
+    and_then_or: "p&q|r", "pq&r|",
+    or_then_and: "p|q&r", "pqr&|",
+    ignore_spaces: "p & q", "pq&",
+    long_implication: "p = > q", "pq>",
 }
 
 #[test]
